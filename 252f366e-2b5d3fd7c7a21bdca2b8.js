e385b2bfd69214678f24ca6c630ace26d4060d7d
ahsan8244 <syed.ahsan8244@gmail.com>
2021-09-07 02:58:39 +0800

    Updates

---
+++ 252f366e-2b5d3fd7c7a21bdca2b8.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkme=self.webpackChunkme||[]).push([[228],{2775:function(t,c,e){e.d(c,{ixJ:function(){return u}});var n=e(1046);function u(t){return(0,n.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{d:"M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z"}}]})(t)}}}]);
+//# sourceMappingURL=252f366e-2b5d3fd7c7a21bdca2b8.js.map
